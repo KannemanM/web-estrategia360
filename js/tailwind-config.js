2ab517d87c74fce26125057d85444ffed3ef944d
kann3man <martin.kanneman@gmail.com>
2025-12-03 13:55:48 -0300

    upload web

---
+++ js/tailwind-config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                brand: {
+                    dark: '#041028', // Slate 900
+                    blue: '#203e66', // Blue 800
+                    accent: '#5c5d5e', // Blue 500
+                    light: '#bbbdc3', // Slate 50
+                }
+            }
+        }
+    }
+}
